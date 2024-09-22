be9f67450ab0c0fb841d92a44b385c1941d6dc18
yystephan <97153671+yykido@users.noreply.github.com>
2024-09-22 23:26:34 +0200

    added the picture of the logo and new photo

--- my-portfolio/src/components/MySkill.tsx
+++ my-portfolio/src/components/MySkill.tsx
@@ -6,15 +6,18 @@ interface Skill {
 }
 
 const skills: Skill[] = [
-  { name: 'JAVASCRIPT', icon: 'js-icon.png' },
-  { name: 'TYPESCRIPT', icon: 'ts-icon.png' },
-  { name: 'FIREBASE', icon: 'firebase-icon.png' },
-  { name: 'REACT JS/TS', icon: 'react-icon.png' },
-  { name: 'HTML', icon: 'html-icon.png' },
-  { name: 'CSS', icon: 'css-icon.png' },
-  { name: 'SCSS', icon: 'scss-icon.png' },
-  { name: 'GIT', icon: 'git-icon.png' },
-  { name: 'REACT-ROUTER-DOM', icon: 'react-router-icon.png' },
+  { name: 'JAVASCRIPT', icon: 'https://static-00.iconduck.com/assets.00/javascript-js-icon-2048x2048-nyxvtvk0.png' },
+  { name: 'TYPESCRIPT', icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968381.png' },
+  { name: 'REACT', icon: 'https://res.cloudinary.com/practicaldev/image/fetch/s--qo_Wp38Z--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/e0nl7ziy1la7bpwj7rsp.png' },
+  { name: 'JAVA', icon: 'https://static-00.iconduck.com/assets.00/java-icon-1511x2048-6ikx8301.png' },
+  { name: 'SPRING BOOT', icon: 'https://img.icons8.com/color/200/spring-logo.png' },
+  { name: 'REDIS', icon: 'https://cdn4.iconfinder.com/data/icons/redis-2/1451/Untitled-2-512.png' },
+  { name: 'MYSQL', icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968313.png' },
+  { name: 'MONGODB', icon: 'https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg' },
+  { name: 'ELASTICSEARCH', icon: 'https://static-00.iconduck.com/assets.00/elasticsearch-icon-1839x2048-s0i8mk51.png' },
+  { name: 'GIT', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png' },
+  { name: 'AWS', icon: 'https://cmscritic.com/ms-content/uploads/2024/03/aws-logo-icon.jpg?format=auto&width=256' },
+  { name: 'DOCKER', icon: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png' },
 ];
 
 const additionalSkills = [
@@ -31,7 +34,7 @@ const MySkill: React.FC = () => {
           <div className="skills-grid">
             {skills.map((skill, index) => (
               <div key={index} className="skill-item">
-                <img src={`/icons/${skill.icon}`} alt={skill.name} className="skill-icon" />
+                <img src={`${skill.icon}`} alt={skill.name} className="skill-icon" />
                 <span>{skill.name}</span>
               </div>
             ))}
@@ -43,7 +46,7 @@ const MySkill: React.FC = () => {
           <button className="resume-button">Get my resume</button>
         </div>
         <div className="skills-image">
-          <img src="/profile-working.jpg" alt="Profile working" />
+          <img src="/profile-work.jpg" alt="Profile working" />
         </div>
       </div>
     </section>
